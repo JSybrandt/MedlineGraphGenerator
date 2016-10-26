ae6fd4eb2aa721aa2e18425d7d92c1829be95cf1
JSybrandt <nalta45@gmail.com>
2016-10-25 23:27:58 -0400

    added file details

--- main.cpp
+++ main.cpp
@@ -5,8 +5,8 @@
 #include<unordered_map>
 #include<fstream>
 #include<regex>
-#include<algorithm> 
-#include<functional> 
+#include<algorithm>
+#include<functional>
 #include<cctype>
 #include<locale>
 #include<sstream>
@@ -63,7 +63,7 @@ vector<string> getFilesInDir(string dirPath){
 //Returns PMID, AbstractText pairs
 //second param are all the values we already have
 map<string,string> parseXML(string fileName, const unordered_map<string,string>& backup){
-    
+
     map<string,string> pmid2abstract;
     regex abstractRegex(ABSTRACT_REGEX,regex_constants::ECMAScript);
     regex pmidRegex(PMID_REGEX,regex_constants::ECMAScript);
@@ -71,7 +71,7 @@ map<string,string> parseXML(string fileName, const unordered_map<string,string>&
     string line;
     string lastFoundPMID = "NULL";
     string lastFoundAbstract = "";
-    
+
     while(getline(fin,line)){
         if(regex_search(line,pmidRegex)){
             lastFoundAbstract = trim(lastFoundAbstract);
@@ -88,7 +88,7 @@ map<string,string> parseXML(string fileName, const unordered_map<string,string>&
     //Need extra check at end
     if(lastFoundPMID != "NULL" && lastFoundAbstract != "")
         pmid2abstract[lastFoundPMID] = lastFoundAbstract;
-    
+
     fin.close();
     return pmid2abstract;
 }
@@ -111,11 +111,13 @@ void parseMedline(unordered_map<string,string>& pmid2abstract, string dirPath, f
     int completeCount = 0;
 #pragma omp parallel  for
     for(int i = 0 ; i < xmlPaths.size();i++){
-        
+
         map<string,string> tmp = parseXML(xmlPaths[i],pmid2abstract);
-                
+
         if(tmp.size() > 1){
-                       
+#pragma omp critical (LOGGER)
+          lout << "New: " << xmlPaths[i]<<endl;
+
              stringstream s;
             int BASH_SIZE = 50;
 
@@ -138,22 +140,25 @@ void parseMedline(unordered_map<string,string>& pmid2abstract, string dirPath, f
 
         #pragma omp critical (INSERT_ABSTRACT)
             pmid2abstract.insert(tmp.begin(),tmp.end());
+        } else{
+#pragma omp critical (LOGGER)
+          lout<<"Skip:" << xmlPaths[i]<<endl;
         }
-        
-           
+
+
         #pragma omp critical (LOGGER)
         {
             completeCount++;
             lout << (completeCount / (double) xmlPaths.size()) * 100 << "%" << endl;
         }
     }
-       
+
 }
 
 void runFlann(unordered_map<string,Vec>& pmid2vec, vector<string>& pmids, string outputFilePath){
     //row major order, map.size() rows and VECTOR_SIZE cols
     float * vecData = new float[pmid2vec.size()*VECTOR_SIZE];
-    
+
     int pmidCount = 0;
     for(string pmid : pmids){
         for(int i = 0 ; i < VECTOR_SIZE; i++){
@@ -161,29 +166,29 @@ void runFlann(unordered_map<string,Vec>& pmid2vec, vector<string>& pmids, string
         }
         pmidCount++;
     }
-        
+
     flann::Matrix<float> data(vecData,pmid2vec.size(),VECTOR_SIZE);
-      
+
     flann::Index<flann::L2<float> > index(data, flann::KDTreeIndexParams(16));
     index.buildIndex();
-    
+
     vector<vector<int> > indicies;
     vector<vector<float> > dists;
-    
+
     flann::SearchParams params(128);
     params.cores = 0; //automatic core selection
     index.knnSearch(data, indicies, dists, 10, params);
- 
+
     fstream fout(outputFilePath,ios::out);
-    
+
     for(int currIndex = 0; currIndex < indicies.size(); ++currIndex){
         string pmid = pmids[currIndex];
         for(int nIndex = 0; nIndex < indicies[currIndex].size(); ++nIndex){
             int neighborID = indicies[currIndex][nIndex];
             string nPMID = pmids[neighborID];
-            
+
             float pairDist = dists[currIndex][nIndex];
-            
+
             fout<<pmid << " " << nPMID << " " <<pairDist<< endl;
         }
     }
@@ -193,7 +198,7 @@ void runFlann(unordered_map<string,Vec>& pmid2vec, vector<string>& pmids, string
 
 void loadOldCanon(unordered_map<string,string>& pmid2canon){
     vector<string> xmlPaths = getFilesInDir(BACKUP_FILES_DIR);
-    
+
     #pragma omp parallel  for
     for(int i = 0 ; i < xmlPaths.size();i++){
         unordered_map<string,string> tmpMap;
@@ -209,7 +214,7 @@ void loadOldCanon(unordered_map<string,string>& pmid2canon){
         }
 #pragma omp critical (LOAD_CANON_LOCK)
         pmid2canon.insert(tmpMap.begin(),tmpMap.end());
-        
+
         res.close();
     }
 }
@@ -223,44 +228,44 @@ void catchChild(int sigNum){
 int main(int argc, char** argv) {
 
     signal(SIGCHLD, catchChild);
-    
+
     fstream lout(LOG_FILE,ios::out);
     lout<<"Started"<<endl;
-    
+
     unordered_map<string,string> pmid2abstract;
-    
+
     lout<<"Recovering Lost Data"<<endl;
     loadOldCanon(pmid2abstract);
-    
-    lout<<"Recovered "<< pmid2abstract.size() << " old records" << endl; 
-    
+
+    lout<<"Recovered "<< pmid2abstract.size() << " old records" << endl;
+
     lout<<"Parsing MEDLINE XML"<<endl;
     parseMedline(pmid2abstract,MEDLINE_XML_DIR,lout);
-    
+
     lout<<"Found " << pmid2abstract.size() << " total abstracts"<<endl;
-    
+
     vector<string> pmids;
     for(auto val : pmid2abstract){
         pmids.push_back(val.first);
     }
-    
+
     fstream canonOut(CANON_FILE,ios::out);
-    
+
     for(auto val : pmid2abstract){
         canonOut << val.second<<endl;
     }
-    
+
     canonOut.close();
-    
+
     lout<<"Training Fast Text"<<endl;
     lout<<"Running " << FASTTEXT_COMMAND << endl;
     system(FASTTEXT_COMMAND.c_str());
-    
+
     lout<<"Building Dict"<<endl;
     Dict dict(VECTOR_FILE);
-    
+
     unordered_map<string,Vec> pmid2vec;
-    
+
     lout<<"Getting vectors per abstract"<<endl;
 #pragma omp parallel for
     for(int i = 0 ; i < pmids.size();i++){
@@ -283,10 +288,10 @@ int main(int argc, char** argv) {
 #pragma omp critical (SET_PMID_VEC)
         pmid2vec[pmid] = vec;
     }
-    
+
     lout<<"Running FLANN"<<endl;
     runFlann(pmid2vec,pmids, OUTPUT_FILE);
-    
+
     lout << "DONE!" << endl;
 
     lout.close();
