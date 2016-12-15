a540efaab4064b103ce176fa2e936a0ac06fb9fb
JSybrandt <nalta45@gmail.com>
2016-12-15 16:31:51 -0500

    fixed lvg issue

--- bin/lvgCommand.sh
+++ bin/lvgCommand.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-lvg -CR:o:oc:oe:oi -R:1 -f:q:q0:q1:q2:q3:q4:q5:q6:q7:q8:rs:T:l:t:g:P:g:C | awk 'BEGIN{FS="|"}{print $2}'
+lvg -CR:o:oc:oe:oi -R:1 -f:q:q0:q1:q2:q5:q7:q8:rs:T:l:t:g:P:g:C | awk 'BEGIN{FS="|"}{print $2}'
